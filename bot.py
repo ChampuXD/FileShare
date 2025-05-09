79728f20fb8b111f1d6ed3aafe5a313df2e46d24
ᴛʜᴇ ᴄʜᴧᴍᴘᴜ <112340896+ChampuXD@users.noreply.github.com>
2025-05-09 10:48:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 from aiohttp import web
 from plugins import web_server
-import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
-from config import API_HASH, API_ID, LOGGER, BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+from config import API_HASH, API_ID, LOGGER, BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT
 import pyrogram.utils
 
 pyrogram.utils.MIN_CHANNEL_ID = -1001218085896
-
 
 
 class Bot(Client):
@@ -29,29 +27,10 @@ class Bot(Client):
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
 
-        if FORCE_SUB_CHANNEL:
-            try:
-                self.LOGGER(__name__).info(f"Attempting to access FORCE_SUB_CHANNEL: {FORCE_SUB_CHANNEL}")
-                chat = await self.get_chat(FORCE_SUB_CHANNEL)
-                link = chat.invite_link
-                if not link:
-                    self.LOGGER(__name__).info("No invite link found, attempting to export...")
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                self.invitelink = link
-                self.LOGGER(__name__).info(f"Invite link successfully retrieved: {self.invitelink}")
-            except Exception as a:
-                self.LOGGER(__name__).error(f"Error accessing FORCE_SUB_CHANNEL: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).error(f"Exception details: {a}")
-                self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped...")
-                sys.exit()
-
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Hey 🖐")
+            test = await self.send_message(chat_id=db_channel.id, text="Hey 🖐")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
@@ -62,7 +41,7 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running...!")
         self.username = usr_bot_me.username
-        #web-response
+        # web-response
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
@@ -71,4 +50,3 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot Stopped...")
-            
